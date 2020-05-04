e0b36c5ad828a09acd6ff68ae8dc65bb7eb814ab
devanshsethia <dsethia2@illinois.edu>
2020-05-05 03:50:55 +0530

    XML Updated

--- app/src/main/java/com/example/finalproject/FormActivity.java
+++ app/src/main/java/com/example/finalproject/FormActivity.java
@@ -1,6 +1,7 @@
 package com.example.finalproject;
 
 import android.app.AlertDialog;
+import android.app.ProgressDialog;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.SharedPreferences;
@@ -9,28 +10,53 @@ import android.util.JsonReader;
 import android.view.View;
 import android.webkit.WebView;
 import android.widget.EditText;
+import android.widget.TableLayout;
 import android.widget.TextView;
 import android.widget.Button;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import com.android.volley.DefaultRetryPolicy;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.RetryPolicy;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
 
 import org.json.JSONArray;
 import org.json.JSONObject;
 
+import java.util.List;
+import java.util.Map;
+
 public class FormActivity extends AppCompatActivity {
 
-    private String response = "";
+    private Map<String, String> param;
     private JSONObject form;
     private SharedPreferences savedInfo;
-    EditText userDetail;
+    private SharedPreferences.Editor editor;
+    private EditText ask1, ask2, ask3, ask4, ask5, ask6, ask7;
+    private TextView field1, field2, field3, field4, field5, field6, field7;
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_form);
+        setUpTable();
+        Button submit = (Button) findViewById(R.id.buttonX);
+        submit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view)
+            {
+                compile();
+            }
+        });
         savedInfo = getSharedPreferences("UserData", MODE_PRIVATE);
-        SharedPreferences.Editor editor = savedInfo.edit();
+        editor = savedInfo.edit();
         Intent intent = getIntent();
         String json = intent.getStringExtra("Json");
         try {
@@ -43,33 +69,23 @@ public class FormActivity extends AppCompatActivity {
 
     public void read(JSONObject input) {
         try {
-            JSONArray info = form.getJSONArray("form");
+            JSONArray info = form.getJSONArray("Form");
             for (int i = 0; i < info.length(); i++) {
                 String field = info.getString(i);
-                fill(field);
+                getField(i).setText(field + " : ");
+                fill(field, i);
             }
         } catch (Exception e) {
-            showAlertDialog("Error, Try Again");
+            showAlertDialog("Form too Big");
         }
     }
 
-    public void fill(String input) {
+    public void fill(String input, int a) {
         if (savedInfo.contains(input)) {
-            response = response + savedInfo.getString(input, "Default") + ", ";
+            getAsk(a).setHint(savedInfo.getString(input, "Default"));
         } else {
             // create a Edit Text in the Layout to get Data from the user.
-            Button submitInfo;
-            submitInfo = (Button)findViewById(R.id.buttonX);
-
-            TextView ask = (TextView)findViewById(R.id.textViewA);
-            ask.setText("Please enter your " + input);
-            submitInfo.setOnClickListener(new View.OnClickListener() {
-                public void onClick(View view) {
-                   userDetail = (EditText)findViewById(R.id.editTextX);
-                }
-            });
-            response = response + userDetail.getText().toString();
-            //not sure what to do after this
+            getAsk(a).setHint("");
         }
     }
 
@@ -87,4 +103,147 @@ public class FormActivity extends AppCompatActivity {
         AlertDialog alertDialog = builder.create();
         alertDialog.show();
     }
+
+    public void setUpTable() {
+        ask1 = (EditText) findViewById(R.id.ask1);
+        ask1.setVisibility(View.INVISIBLE);
+        ask2 = (EditText) findViewById(R.id.ask2);
+        ask2.setVisibility(View.INVISIBLE);
+        ask3 = (EditText) findViewById(R.id.ask3);
+        ask3.setVisibility(View.INVISIBLE);
+        ask4 = (EditText) findViewById(R.id.ask4);
+        ask4.setVisibility(View.INVISIBLE);
+        ask5 = (EditText) findViewById(R.id.ask5);
+        ask5.setVisibility(View.INVISIBLE);
+        ask6 = (EditText) findViewById(R.id.ask6);
+        ask6.setVisibility(View.INVISIBLE);
+        ask7 = (EditText) findViewById(R.id.ask7);
+        ask7.setVisibility(View.INVISIBLE);
+
+        field1 = (TextView) findViewById(R.id.field1);
+        field1.setVisibility(View.INVISIBLE);
+        field2 = (TextView) findViewById(R.id.field2);
+        field2.setVisibility(View.INVISIBLE);
+        field3 = (TextView) findViewById(R.id.field3);
+        field3.setVisibility(View.INVISIBLE);
+        field4 = (TextView) findViewById(R.id.field4);
+        field4.setVisibility(View.INVISIBLE);
+        field5 = (TextView) findViewById(R.id.field5);
+        field5.setVisibility(View.INVISIBLE);
+        field6 = (TextView) findViewById(R.id.field6);
+        field6.setVisibility(View.INVISIBLE);
+        field7 = (TextView) findViewById(R.id.field7);
+        field7.setVisibility(View.INVISIBLE);
+    }
+
+    public EditText getAsk(int a) {
+        switch (a) {
+        case 1:
+            ask1.setVisibility(View.VISIBLE);
+            return ask1;
+        case 2:
+            ask2.setVisibility(View.VISIBLE);
+            return ask2;
+        case 3:
+            ask3.setVisibility(View.VISIBLE);
+            return ask3;
+        case 4:
+            ask4.setVisibility(View.VISIBLE);
+            return ask4;
+        case 5:
+            ask5.setVisibility(View.VISIBLE);
+            return ask5;
+        case 6:
+            ask6.setVisibility(View.VISIBLE);
+            return ask6;
+        default:
+            ask7.setVisibility(View.VISIBLE);
+            return ask7;
+        }
+    }
+
+    public TextView getField(int a) {
+        switch (a) {
+        case 1:
+            field1.setVisibility(View.VISIBLE);
+            return field1;
+        case 2:
+            field2.setVisibility(View.VISIBLE);
+            return field2;
+        case 3:
+            field3.setVisibility(View.VISIBLE);
+            return field3;
+        case 4:
+            field4.setVisibility(View.VISIBLE);
+            return field4;
+        case 5:
+            field5.setVisibility(View.VISIBLE);
+            return field5;
+        case 6:
+            field6.setVisibility(View.VISIBLE);
+            return field6;
+        default:
+            field7.setVisibility(View.VISIBLE);
+            return field7;
+        }
+    }
+
+    public void compile() {
+        try {
+            int size = form.getJSONArray("Form").length();
+            for (int i = 0; i < size; i++) {
+                if (getAsk(i).getText().toString().isEmpty()) {
+                    param.put(getField(i).getText().toString(), getAsk(i).getHint().toString());
+                } else {
+                    param.put(getField(i).getText().toString(), getAsk(i).getText().toString());
+                    editor.putString(getField(i).getText().toString(), getAsk(i).getText().toString());
+                }
+            }
+            editor.apply();
+        } catch (Exception e) {
+            showAlertDialog("Oops!");
+        }
+        addItemToSheet();
+    }
+    private void   addItemToSheet() {
+
+        final ProgressDialog loading = ProgressDialog.show(this,"Adding Item","Please wait");
+
+
+        StringRequest stringRequest = new StringRequest(Request.Method.POST, "https://docs.google.com/spreadsheets/d/13_J5kXufZumPFNpdm5pWf0HHRLrZMaMdC57VODuusv4/edit#gid=0",
+                new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+                        loading.dismiss();
+                        Toast.makeText(FormActivity.this, response,Toast.LENGTH_LONG).show();
+                        Intent intent = new Intent(getApplicationContext(),MainActivity.class);
+                        startActivity(intent);
+
+                    }
+                },
+                new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+
+                    }
+                }
+        ) {
+            @Override
+            protected Map<String, String> getParams() {
+                param.put("action", "addItem");
+                return param;
+            }
+        };
+
+        int socketTimeOut = 50000;// u can change this .. here it is 50 seconds
+
+        RetryPolicy retryPolicy = new DefaultRetryPolicy(socketTimeOut, 0, DefaultRetryPolicy.DEFAULT_BACKOFF_MULT);
+        stringRequest.setRetryPolicy(retryPolicy);
+
+        RequestQueue queue = Volley.newRequestQueue(this);
+
+        queue.add(stringRequest);
+
+    }
+
 }
--- app/src/main/java/com/example/finalproject/RegisterActivity.java
+++ app/src/main/java/com/example/finalproject/RegisterActivity.java
@@ -1,6 +1,11 @@
 package com.example.finalproject;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.content.SharedPreferences.Editor;
 import android.view.View;
@@ -20,17 +25,42 @@ public class RegisterActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
-        mButton = (Button)findViewById(R.id.button1);
-
+        mEditUser   = (EditText)findViewById(R.id.editText1);
+        mEditEmail = (EditText)findViewById(R.id.editText2);
+        mEditMajor = (EditText)findViewById(R.id.editText3);
+        mEditDOB = (EditText)findViewById(R.id.editText4);
+        mText = (TextView)findViewById(R.id.textView1);
+        mButton = (Button) findViewById(R.id.button1);
         mButton.setOnClickListener(new View.OnClickListener() {
             public void onClick(View view) {
-                mEditUser   = (EditText)findViewById(R.id.editText1);
-                mEditEmail = (EditText)findViewById(R.id.editText2);
-                mEditMajor = (EditText)findViewById(R.id.editText3);
-                mEditDOB = (EditText)findViewById(R.id.editText4);
-                mText = (TextView)findViewById(R.id.textView1);
-                mText.setText("Welcome "+mEditUser.getText().toString()+"!");
+                SharedPreferences savedInfo = getSharedPreferences("UserData", 0);
+                SharedPreferences.Editor editor = savedInfo.edit();
+                editor.putString("Name", mEditUser.getText().toString());
+                editor.putString("Email", mEditEmail.getText().toString());
+                editor.putString("Major", mEditMajor.getText().toString());
+                editor.putString("DOB", mEditDOB.getText().toString());
+                editor.putBoolean("firstTime", false);
+                editor.apply();
+                showAlertDialog("Welcome "+mEditUser.getText().toString()+"!");
             }
         });
+    }
+    private void showAlertDialog(String message){
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setMessage(message);
+        builder.setCancelable(true);
+
+        builder.setPositiveButton(
+                "SCAN CODE",
+                new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int id) {
+                        dialog.cancel();
+                        Intent intent = new Intent(RegisterActivity.this, MainActivity.class);
+                        startActivity(intent);
+                    }
+                });
+
+        AlertDialog alertDialog = builder.create();
+        alertDialog.show();
     }
 }
