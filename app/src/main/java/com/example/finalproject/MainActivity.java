f2a860fa9f9dd7421a545056accbc007b5ab63f9
kaustabhsharma <ksharma8@illinois.edu>
2020-05-04 02:26:15 -0500

    did some of RegisterActivity- still in progress

--- app/src/main/java/com/example/finalproject/MainActivity.java
+++ app/src/main/java/com/example/finalproject/MainActivity.java
@@ -45,6 +45,8 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         if (checkFirstTime()) {
             // Pass Activity to Register Page.
+            Intent intent = new Intent(MainActivity.this, RegisterActivity.class);
+            startActivity(intent);
         }
 
 
---
+++ app/src/main/java/com/example/finalproject/RegisterActivity.java
@@ -0,0 +1,34 @@
+package com.example.finalproject;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+import android.content.SharedPreferences.Editor;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class RegisterActivity extends AppCompatActivity {
+    Button mButton;
+    EditText mEditUser;
+    EditText mEditEmail;
+    EditText mEditMajor;
+    EditText mEditDOB;
+    TextView mText;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register);
+        mButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View view) {
+                mEditUser   = (EditText)findViewById(R.id.editText1);
+                mEditEmail = (EditText)findViewById(R.id.editText2);
+                mEditMajor = (EditText)findViewById(R.id.editText3);
+                mEditDOB = (EditText)findViewById(R.id.editText4);
+                mText = (TextView)findViewById(R.id.textView1);
+                mText.setText("Welcome "+mEditUser.getText().toString()+"!");
+            }
+        });
+    }
+}
